29b81026f86be553208b7a66f8ea7599c7b961de
Adam 'The Pharaoh' Hoy <67761852+AddyTheCoder@users.noreply.github.com>
2022-03-15 19:33:10 -0400

    editing some basic styles to a DOM

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+	dogNames[i].style.textAlign = "right" 
+}
+
+let footer = document.querySelector('#footer')
+footer.style.color = "green"
